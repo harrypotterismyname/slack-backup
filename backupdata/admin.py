296d73b36297d9beb5d05b61d1cbb5848874b7eb
Hong <hong@cloudjetsolutions.com>
2015-01-03 16:51:25 +0700

    admin

--- backupdata/admin.py
+++ backupdata/admin.py
@@ -1,3 +1,7 @@
+from backupdata.models import Channel, Message, ChannelMember
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Channel)
+admin.site.register(Message)
+admin.site.register(ChannelMember)
